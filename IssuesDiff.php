5c812a6140ce6a339bffa251070d70cfd7fef5e1
Samir Aguiar <samir.aguiar@intra2net.com>
2017-07-28 07:37:39 -0300

    Merge licensing notices

--- IssuesDiff.php
+++ IssuesDiff.php
@@ -1,11 +1,11 @@
 <?php
 
-# IssuesDiff - a MantisBT plugin that adds a visual diff between revisions
-#
-# You should have received a copy of the GNU General Public License
-# along with IssuesDiff.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
+ * IssuesDiff - a MantisBT plugin that adds a visual diff between revisions
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IssuesDiff.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @copyright Copyright (C) 2017 Samir Aguiar for Intra2net AG - www.intra2net.com
  */
 
